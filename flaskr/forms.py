aa1d90a0c5c44d603eb9e619c4e93637b0a73502
Carl Steinbach <Steinbachcf01@gmail.com>
2022-10-11 11:08:25 +0200

    login form

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -1,6 +1,10 @@
+from ast import Pass
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    email = StringField('e-mail', validators=[DataRequired])
+    name = StringField('name', validators=[DataRequired()])
+    email = StringField('e-mail', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,5 +1,6 @@
-from flask import current_app as app
+from flask import current_app as app, request, url_for, redirect, flash
 from flask import render_template
+from flaskr.forms import LoginForm
 
 @app.route('/')
 def index():
@@ -11,13 +12,17 @@ def home():
     # the homepage of a user displaying his notes
     return render_template('home.html', title='Home')
     
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     # the login page
-    return render_template('login.html', title='Login')
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('now logged in', 'message')
+        return redirect(url_for(home))
+
+    return render_template('login.html', title='Login', form=form)
 
 @app.route('/register')
 def register():
     # the register page
     return render_template('register.html', title='Register')
-    

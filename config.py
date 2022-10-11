5960d4865671d60d7f7803ebce0ac3a45ff2755e
Carl Steinbach <Steinbachcf01@gmail.com>
2022-10-11 13:38:39 +0200

    recaptcha

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ load_dotenv(path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = environ.get('SECRET_KEY')
+    RECAPTCHA_PUBLIC_KEY = environ.get('RECAPTCHA_PUBLIC_KEY')
+    RECAPTCHA_SECRET_KEY = environ.get('RECAPTCHA_SECRET_KEY')
     TEMPLATES_FOLDER = 'templates'
     STATIC_FOLDER = 'static'
 

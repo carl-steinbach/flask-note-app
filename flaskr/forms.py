3184536f3a6bc963f088422977ca6f65719d6e37
Carl Steinbach <Steinbachcf01@gmail.com>
2022-10-19 21:16:24 +0200

    requirements

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -3,7 +3,8 @@ from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class LoginForm(FlaskForm):
-    email = StringField('e-mail', validators=[DataRequired(), Email('invalid email address')])
+    email = StringField('e-mail', validators=[DataRequired(), 
+                                            Email('invalid email address')])
     password = PasswordField('password', validators=[DataRequired()])
     recaptcha = RecaptchaField()
     stay_logged_in = BooleanField('stay logged in')
@@ -11,8 +12,10 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
-    email = StringField('e-mail', validators=[DataRequired(), Email('invalid email address')])
+    email = StringField('e-mail', validators=[DataRequired(), 
+                                            Email('invalid email address')])
     password = PasswordField('password', validators=[DataRequired()])
-    repeat_password = PasswordField('password', validators=[DataRequired(), EqualTo('password')])
+    repeat_password = PasswordField('password', validators=[DataRequired(), 
+                                                            EqualTo('password')])
     recaptcha = RecaptchaField()
     submit = SubmitField('Submit')
